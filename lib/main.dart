5fa5f21811b30b62274e45e633c6e12c6a65cffd
Ri-Hong <riri.hong@gmail.com>
2023-10-08 01:28:26 -0400

    Added loading

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,11 @@ void main() {
 }
 
 class Message {
-  final String text;
+  String text;
   final bool isUser; // true if the message is from the user, false if from the chatbot
+  bool isLoading; // New attribute to track loading state for each message
 
-  Message({required this.text, required this.isUser});
+  Message({required this.text, required this.isUser, this.isLoading = false});
 }
 
 class MyApp extends StatelessWidget {
@@ -38,13 +39,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<Message> _messages = [];
   final TextEditingController _textController = TextEditingController();
+  bool isLoading = false; // New state variable
 
   void _sendMessage() {
     if (_textController.text.isNotEmpty) {
       setState(() {
         _messages.insert(0, Message(text: _textController.text, isUser: true));
-        _messages.insert(0, Message(text: "Bot reply to: ${_textController.text}", isUser: false));
+        _messages.insert(0, Message(text: "Loading...", isUser: false, isLoading: true)); // Set isLoading to true for the bot's response
       });
+      
+      // Simulate a delay for the loading animation
+      Future.delayed(Duration(seconds: 5), () {
+        setState(() {
+          _messages[0].isLoading = false; // Stop loading animation for the bot's response
+          _messages[0].text = "Bot reply to: ${_textController.text}";
+        });
+      });
+
       _textController.clear();
     }
   }
@@ -71,18 +82,44 @@ class _MyHomePageState extends State<MyHomePage> {
                 final message = _messages[index];
                 return Align(
                   alignment: message.isUser ? Alignment.centerRight : Alignment.centerLeft,
-                  child: Container(
-                    margin: EdgeInsets.all(8.0),
-                    padding: EdgeInsets.all(12.0),
-                    decoration: BoxDecoration(
-                      color: message.isUser ? Color.fromARGB(255, 36, 213, 190) : Color.fromARGB(255, 148, 151, 167),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Text(
-                      message.text,
-                      style: TextStyle(color: Colors.black),
+                  child: IntrinsicWidth( // Add this widget
+                    child: Container(
+                      margin: EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(12.0),
+                      decoration: BoxDecoration(
+                        color: message.isUser ? Color.fromARGB(255, 167, 238, 229) : Color.fromARGB(255, 233, 233, 233),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: message.isUser
+                        ? Text(
+                            message.text,
+                            style: TextStyle(color: Colors.black),
+                            textAlign: TextAlign.right,
+                          )
+                        : message.isLoading
+                            ? Container(
+                                width: 30.0,  // Set the desired width
+                                height: 30.0, // Set the desired height
+                                child: Image.asset('assets/loading.gif'),
+                              )
+                            : Row(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset('assets/OdooSalesIcon.png', fit: BoxFit.cover, height: 30,),
+                                  SizedBox(width: 8.0),
+                                  ConstrainedBox(
+                                    constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.7),
+                                    child: Text(
+                                      message.text,
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                ],
+                              ),
                     ),
                   ),
+  
                 );
               },
             ),
@@ -131,8 +168,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                         ),
                       ),
-
-
 
                     ),
                     onSubmitted: (text) {
